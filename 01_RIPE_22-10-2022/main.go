8c36da3dd04c4804c2ccda76e8dad0f64d035b81
ShadowGabbo <gabriele.sarti1@studenti.unimi.it>
2022-10-30 10:19:56 +0100

    Added code of lezione1

---
+++ 01_RIPE_22-10-2022/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// dichiarare variabili
+	var s string
+	var x int
+	/*
+		var b bool
+		var f float64
+		var c rune
+	*/
+
+	// inizializzazione variabili
+	s = "ciao"
+	x = 4
+	/*
+		b = true
+		f = 1.4
+		c = 'c'
+	*/
+
+	// oppure insieme
+	var z int = 10
+	// var somma int = z + x //14
+
+	// inferenza di tipo
+	//y := "pippo"
+
+	// tipo di una variabile
+	// fmt.Println(reflect.TypeOf(y))
+
+	// esempi
+	s2 := "simo"
+
+	// output
+	fmt.Println(s + s2) //concatenazione
+	fmt.Println(s, s2)  // dopo , aggiunge uno spazio e va a capo
+	fmt.Print(s, s2)    // dopo , non aggiuge uno spazio e non va a capo
+	// operazioni con varibili
+	fmt.Printf("\nLa somma tra %d + %d è: %d.\n", x, z, x+z)
+
+	// standard input
+	var numero1 int
+	fmt.Scan(&numero1)
+	fmt.Printf("Il numero inserito è: %d.\n", numero1)
+
+	// redirezione dell'input go run <nomefile>.go < <nomefileinput>.txt
+}
